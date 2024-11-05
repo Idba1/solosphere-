e34cca35a37b9f6d6a0ba8d8027936f45420f9bd
Idba1 <moniraislamself@gmail.com>
2024-11-05 19:38:13 +0600

    update login

--- client/src/Pages/Authentication/LogIn.jsx
+++ client/src/Pages/Authentication/LogIn.jsx
@@ -1,8 +1,45 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../Provider/AuthProvider"
+import toast from "react-hot-toast";
 
 const Login = () => {
+    const { signIn, signInWithGoogle, } = useContext(AuthContext);
+    const navigate = useNavigate()
+
+
+    // google login
+    const handleGooleSignIn = async () => {
+        try {
+            await signInWithGoogle()
+            toast.success('Sign in successfully!')
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+            toast.error(error?.message)
+        }
+    }
+
+    // email and Password
+    const handleSignInEmailPass = async e => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value
+        const pass = form.password.value
+        console.log({ email, pass })
+        try {
+            //User Login
+            const result = await signIn(email, pass)
+            console.log(result)
+            navigate('/')
+            toast.success('Signin Successful')
+        } catch (err) {
+            console.log(err)
+            toast.error(err?.message)
+        }
+    }
     return (
-        <div className='flex justify-center items-center min-h-[calc(100vh-306px)]'>
+        <div className='flex justify-center items-center my-12 min-h-[calc(100vh-306px)]'>
             <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
                 <div
                     className='hidden bg-cover bg-center lg:block lg:w-1/2'
@@ -24,7 +61,7 @@ const Login = () => {
                         Welcome back!
                     </p>
 
-                    <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+                    <div onClick={handleGooleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
                         <div className='px-4 py-2'>
                             <svg className='w-6 h-6' viewBox='0 0 40 40'>
                                 <path
@@ -60,7 +97,7 @@ const Login = () => {
 
                         <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
                     </div>
-                    <form>
+                    <form onSubmit={handleSignInEmailPass}>
                         <div className='mt-4'>
                             <label
                                 className='block mb-2 text-sm font-medium text-gray-600 '
