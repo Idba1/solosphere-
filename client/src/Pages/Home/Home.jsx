f14fc8621be8f586229c63f7441bce818aefac38
Idba1 <moniraislamself@gmail.com>
2024-11-06 06:12:47 +0600

    banner section with swiper js and meraki ui components

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Swiperr from "../../Components/Swiper/Carousel";
+
+const Home = () => {
+    return (
+        <div>
+            <Swiperr></Swiperr>
+        </div>
+    );
+};
+
+export default Home;
