4e1db18b38be5cce44d6084daafe668ae07bbe93
Idba1 <moniraislamself@gmail.com>
2024-11-07 16:23:33 +0600

    update job

--- client/src/Pages/Job/UpdateJob.jsx
+++ client/src/Pages/Job/UpdateJob.jsx
@@ -1,8 +1,63 @@
-import { useLoaderData } from "react-router-dom"
+import { useContext, useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../Provider/AuthProvider";
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 const UpdateJob = () => {
     const job = useLoaderData()
     console.log(job);
+    const navigate = useNavigate()
+    const {
+        _id,
+        job_title,
+        deadline,
+        category,
+        min_price,
+        max_price,
+        description,
+    } = job || {}
+    const { user } = useContext(AuthContext)
+    const [startDate, setStartDate] = useState(new Date(deadline) || new Date())
+    const handleFormSubmit = async e => {
+        e.preventDefault()
+        const form = e.target
+        const job_title = form.job_title.value
+        const email = form.email.value
+        const deadline = startDate
+        const category = form.category.value
+        const min_price = parseFloat(form.min_price.value)
+        const max_price = parseFloat(form.max_price.value)
+        const description = form.description.value
+        const jobData = {
+            job_title,
+            deadline,
+            category,
+            min_price,
+            max_price,
+            description,
+            buyer: {
+                email,
+                name: user?.displayName,
+                photo: user?.photoURL,
+            },
+        }
+
+        try {
+            const { data } = await axios.put(
+                `${import.meta.env.VITE_API_URL}/job/${_id}`,
+                jobData
+            )
+            console.log(data)
+            toast.success('Job Data Updated Successfully!')
+            navigate('/my-posted-jobs')
+        } catch (err) {
+            console.log(err)
+            toast.error(err.message)
+        }
+    }
     return (
         <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
             <section className=' p-2 md:p-6 mx-auto bg-white rounded-md shadow-md '>
@@ -10,7 +65,7 @@ const UpdateJob = () => {
                     Update a Job
                 </h2>
 
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
                         <div>
                             <label className='text-gray-700 ' htmlFor='job_title'>
@@ -18,6 +73,7 @@ const UpdateJob = () => {
                             </label>
                             <input
                                 id='job_title'
+                                defaultValue={job_title}
                                 name='job_title'
                                 type='text'
                                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
@@ -33,6 +89,7 @@ const UpdateJob = () => {
                                 type='email'
                                 name='email'
                                 disabled
+                                defaultValue={user?.email}
                                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
                             />
                         </div>
@@ -40,6 +97,11 @@ const UpdateJob = () => {
                             <label className='text-gray-700'>Deadline</label>
 
                             {/* Date picker input field */}
+                            <DatePicker
+                                className='border p-2 rounded-md'
+                                selected={startDate}
+                                onChange={date => setStartDate(date)}
+                            />
                         </div>
 
                         <div className='flex flex-col gap-2 '>
@@ -50,6 +112,7 @@ const UpdateJob = () => {
                                 name='category'
                                 id='category'
                                 className='border p-2 rounded-md'
+                                defaultValue={category}
                             >
                                 <option value='Web Development'>Web Development</option>
                                 <option value='Graphics Design'>Graphics Design</option>
@@ -62,6 +125,7 @@ const UpdateJob = () => {
                             </label>
                             <input
                                 id='min_price'
+                                defaultValue={min_price}
                                 name='min_price'
                                 type='number'
                                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
@@ -74,6 +138,7 @@ const UpdateJob = () => {
                             </label>
                             <input
                                 id='max_price'
+                                defaultValue={max_price}
                                 name='max_price'
                                 type='number'
                                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
@@ -87,6 +152,7 @@ const UpdateJob = () => {
                         <textarea
                             className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
                             name='description'
+                            defaultValue={description}
                             id='description'
                             cols='30'
                         ></textarea>
