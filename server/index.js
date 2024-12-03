f1103ac89dfc6bfc010e5ae4052d3c518710fcc8
Idba1 <moniraislamself@gmail.com>
2024-12-04 04:36:55 +0600

    get all bids request from db for job owner

--- server/index.js
+++ server/index.js
@@ -114,6 +114,15 @@ async function run() {
             const result = await bidscollection.find(query).toArray()
             res.send(result)
         })
+
+
+        // get all bids request from db for job owner
+        app.get('/bid-requests/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { 'buyer.email': email }
+            const result = await bidscollection.find(query).toArray()
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
